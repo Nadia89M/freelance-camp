ed3879ecb4f1c1f4c0e6384da8334495ca78702a
Nadia89M <nadia.mohamed89@gmail.com>
2019-01-24 10:35:21 +0000

    Update package.json and server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/freelance-camp-app'));
+app.use(express.static(__dirname + '/dist/freelance-camp'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/freelance-camp-app/index.html'));
+res.sendFile(path.join(__dirname+'/dist/freelance-camp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
