a00166a3038df15a22aba3901d36c03e749075d1
Nadia89M <nadia.mohamed89@gmail.com>
2019-01-24 10:46:36 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,4 @@
-//Install express server
-const express = require('express');
 const path = require('path');
-
-const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/freelance-camp'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/freelance-camp/index.html'));
+app.get('/*all', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
